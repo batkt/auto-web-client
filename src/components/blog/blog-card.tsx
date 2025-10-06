db4e1efeafb1eb0f1e148dcd875ae2bf3eb02069
Hanguri <hanguri78@gmail.com>
2025-10-06 14:05:14 +0800

    navigation

--- src/components/blog/blog-card.tsx
+++ src/components/blog/blog-card.tsx
@@ -5,6 +5,7 @@ import { FaArrowRight } from "react-icons/fa6";
 import { Card, CardContent } from "@/components/ui/card";
 import { Blog } from "@/lib/types/blog.types";
 import { getImageUrl } from "@/lib/utils";
+import Link from "next/link";
 
 interface BlogCardProps {
   blog: Blog;
@@ -27,20 +28,23 @@ const BlogCard: React.FC<BlogCardProps> = ({ blog }) => {
       </div>
 
       <CardContent className="px-0 pt-4">
-        <h3 className="text-xl font-bold mb-3 text-white transition-colors group-hover:text-primary cursor-pointer">
-          {blog.title}
-        </h3>
+        <Link href={`/blog/${blog._id}`}>
+          <h3 className="text-xl font-bold mb-3 text-white transition-colors group-hover:text-primary cursor-pointer">
+            {blog.title}
+          </h3>
+        </Link>
 
         {/* WHY: HTML-тэй текстийг эвдэлгүйгээр 3 мөрөөр л харуулах */}
         <div
           className="mb-4 text-white overflow-hidden [display:-webkit-box] [-webkit-line-clamp:3] [-webkit-box-orient:vertical]"
           dangerouslySetInnerHTML={{ __html: firstTextBlock?.content || "" }}
         />
-
-        <div className="text-white font-semibold transition-colors flex items-center gap-2 cursor-pointer group-hover:text-red-600">
-          Learn More
-          <FaArrowRight className="size-3" />
-        </div>
+        <Link href={`/blog/${blog._id}`}>
+          <div className="text-white font-semibold transition-colors flex items-center gap-2 cursor-pointer group-hover:text-red-600">
+            Learn More
+            <FaArrowRight className="size-3" />
+          </div>
+        </Link>
       </CardContent>
     </Card>
   );
